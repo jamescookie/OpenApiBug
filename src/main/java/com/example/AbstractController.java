cbe2a28af8bdc25fa6d2bcead692df8f433bdf59
James Cook <james.cookie@gmail.com>
2023-01-09 16:50:03 +0000

    @Hidden on abstract class makes no difference

--- src/main/java/com/example/AbstractController.java
+++ src/main/java/com/example/AbstractController.java
@@ -1,7 +1,9 @@
 package com.example;
 
 import io.micronaut.http.annotation.Get;
+import io.swagger.v3.oas.annotations.Hidden;
 
+@Hidden
 public class AbstractController {
     @Get("/should-be-hidden/one")
     public void methodOne() {
